4a12b6f50e54ecdb063e0b1d5633022c6dc01595
Baraded <borodeda@gmail.com>
2025-02-19 18:19:29 +0300

    Les1_2 done

--- src/main/java/org/example/Lesson1_2.java
+++ src/main/java/org/example/Lesson1_2.java
@@ -1,7 +1,25 @@
 package org.example;
+/*
+Создайте метод checkSumSign(), в теле которого объявите две int переменные а и b, и инициализируйте
+их любыми значениями, которыми захотите. Далее метод должен просуммировать эти переменные,
+и если их сумма больше или равна 0, то вывести в консоль сообщение "Сумма положительная",
+в противном случае "Сумма отрицательная";
+ */
 
 public class Lesson1_2 {
+
     public static void main(String[] args) {
+        checkSumSign();
+    }
+    public static void checkSumSign(){
+        int a = 5;
+        int b = -6;
+        int suma = a +b;
+
+        if (suma >= 0){
+            System.out.println("Сумма положительная");
+        } else
+            System.out.println("Сумма отрицательная");
 
     }
 }
