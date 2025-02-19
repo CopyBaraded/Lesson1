e4869527933ab5da9d9ed1712708388905a41a0b
Baraded <borodeda@gmail.com>
2025-02-19 23:41:54 +0300

    Les1_9 done получилось

--- src/main/java/org/example/Lesson1_9.java
+++ src/main/java/org/example/Lesson1_9.java
@@ -7,10 +7,15 @@ package org.example;
 
 public class Lesson1_9 {
     public static void main(String[] args) {
-        int god = 1983;
-        short t = 4;
-        int visocos = (god % t);
-        boolean d = visocos >  0 && (god / 100) != 0;// переписать, что-то не правильно. попробовать через метод
-        System.out.println(d);
+        int god = 1984;
+        if (visGod(god))
+            System.out.println(god + "год високосный");
+        else
+            System.out.println(god + "год не високосный");
     }
+    public static boolean visGod(int god){
+       return (god % 4 == 0 && god % 100 != 0) || (god % 400 == 0);
+    }
+
+    //boolean d = visocos >  0 && (god / 100) != 0;// переписать, что-то не правильно. попробовать через метод
 }
