a56f1dcbd6531f0ad4cdc54766c5e9303ba5fc8c
Baraded <borodeda@gmail.com>
2025-02-19 23:02:43 +0300

    Les1_7 done

---
+++ src/main/java/org/example/Lesson1_7.java
@@ -0,0 +1,13 @@
+package org.example;
+/*
+Напишите метод, которому в качестве параметра передается целое число.
+Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
+Замечание: ноль считаем положительным числом
+ */
+public class Lesson1_7 {
+    public static void main(String[] args) {
+        int g = -4;
+        boolean f = g < 0;
+        System.out.println(f);
+    }
+}
