b39babb5dda7633e1f3578baccbf5246525148ed
Baraded <borodeda@gmail.com>
2025-02-19 23:42:38 +0300

    Les1_9 done почистил

--- src/main/java/org/example/Lesson1_9.java
+++ src/main/java/org/example/Lesson1_9.java
@@ -9,13 +9,12 @@ public class Lesson1_9 {
     public static void main(String[] args) {
         int god = 1984;
         if (visGod(god))
-            System.out.println(god + "год високосный");
+            System.out.println(god + " год високосный");
         else
-            System.out.println(god + "год не високосный");
+            System.out.println(god + " год не високосный");
     }
     public static boolean visGod(int god){
        return (god % 4 == 0 && god % 100 != 0) || (god % 400 == 0);
     }
 
-    //boolean d = visocos >  0 && (god / 100) != 0;// переписать, что-то не правильно. попробовать через метод
 }
