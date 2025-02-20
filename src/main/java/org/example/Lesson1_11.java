a2420d265ec011523fec94dd279d59187843757f
Baraded <borodeda@gmail.com>
2025-02-20 13:16:53 +0300

    Les1_10 реализуемка мы с тобой, дружок заполнение массива

---
+++ src/main/java/org/example/Lesson1_11.java
@@ -0,0 +1,18 @@
+package org.example;
+/*
+Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 12345678.... 100
+ */
+
+import java.util.Arrays;
+
+public class Lesson1_11 {
+    public static void main(String[] args) {
+        int [] wtf = new int[100];
+        System.out.println(Arrays.toString(wtf));
+        System.out.println(wtf.length);
+        for (int i = 0; i < wtf.length; i++) {
+
+
+        }
+    }
+}
