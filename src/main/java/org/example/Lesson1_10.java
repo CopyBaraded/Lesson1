53c2cacff6c75b4afacb68b0ab838cd43f2ebe97
Baraded <borodeda@gmail.com>
2025-02-20 12:39:05 +0300

    Les1_10 создали метод, чего дальше думаем

---
+++ src/main/java/org/example/Lesson1_10.java
@@ -0,0 +1,22 @@
+package org.example;
+/*
+Задать целочисленный массив, состоящий из элементов 0 и 1.
+Например: [1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
+ */
+
+import java.util.Arrays;
+
+public class Lesson1_10 {
+    public static void main(String[] args) {
+        int[] numb = new int[]{0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1};
+        System.out.println("Первоначчальный массив" + Arrays.toString(numb));
+        //for (int i = 0; i < numb.length; i++) {
+            //System.out.println(Arrays.toString(numb));
+    }
+    public static void reversNumbs(int[] numb){
+        for (int i = 0; i < numb.length; i++) {
+            if
+
+        }
+    }
+}
