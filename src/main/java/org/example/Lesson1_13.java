743d51dacb26b701f31e17494431ef0c566f312f
Baraded <borodeda@gmail.com>
2025-02-20 14:33:06 +0300

    Les1_13 взлетело, не понял как

--- src/main/java/org/example/Lesson1_13.java
+++ src/main/java/org/example/Lesson1_13.java
@@ -12,9 +12,9 @@ public class Lesson1_13 {
     public static void main(String[] args) {
 
         int[] twoMas[] = new int[10][10];
-        for (int i = 0; i < args.length; i++) {
+        for (int i = 0; i < twoMas.length; i++) {
             twoMas[i][i] = 1;
-            twoMas[i][args.length - 1 - i] = 1;
+            twoMas[i][twoMas.length - 1 - i] = 1;
         }
         printMas(twoMas);
 
