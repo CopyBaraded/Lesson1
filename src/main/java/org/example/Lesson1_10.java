e73c7457bf32228654056d7f1dcd243a6371556a
Baraded <borodeda@gmail.com>
2025-02-20 13:04:26 +0300

    Les1_10 done

--- src/main/java/org/example/Lesson1_10.java
+++ src/main/java/org/example/Lesson1_10.java
@@ -9,13 +9,17 @@ import java.util.Arrays;
 public class Lesson1_10 {
     public static void main(String[] args) {
         int[] numb = new int[]{0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1};
-        System.out.println("Первоначчальный массив" + Arrays.toString(numb));
-        //for (int i = 0; i < numb.length; i++) {
-            //System.out.println(Arrays.toString(numb));
+        System.out.println("Первоначальный массив " + Arrays.toString(numb));
+
+        reversNumbs(numb);
+        System.out.println("Перевернутый массив   " + Arrays.toString(numb));
     }
     public static void reversNumbs(int[] numb){
         for (int i = 0; i < numb.length; i++) {
-            if
+            if (numb[i] == 0)
+                numb[i] = numb[i] + 1;
+            else
+                numb[i] = numb[i] - 1;
 
         }
     }
